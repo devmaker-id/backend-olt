faca6adefaa34a0407ecf9bfa985cdf0d3e4a965
devmaker-id <centerbunga@gmail.com>
2026-06-02 11:32:37 +0700

    add web interfaces

---
+++ web/src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+import {
+  RouterProvider
+} from 'react-router-dom'
+
+import { router } from './app/router'
+import { Providers } from './app/providers'
+
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
+  <Providers>
+    <RouterProvider
+      router={router}
+    />
+  </Providers>
+)
